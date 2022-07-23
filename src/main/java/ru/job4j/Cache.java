d6370daf7d613129908c0643709fe23ae20473d1
anton-lapin <lapin_antoine@mail.ru>
2022-07-23 20:00:33 +0300

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/ru/job4j/Cache.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Cache {
+
+    private static Cache cache;
+
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+
+}
